22980afe17f10e88de9e24300d54828aee5a867e
kanliot <kanliot@gmail.com>
2019-05-03 15:07:40 -0400

    Add files via upload

--- show,output_mime.sh
+++ show,output_mime.sh
@@ -1,14 +1,15 @@
+
 # pass a list of files to show what xdg-mime thinks about the mime type of the files
 show_mime_from_files () {
 	(
 	for file 
 	#this doesn't work: MIM="$(file -b --mime-type "$file" )"  it appears to work for most files, but returns wrong type for files ending in m4a
 	do MIM="$(xdg-mime query filetype "$file")"
-		file_s=${file: -16}  #cut the filename so columns of output  look ok
-		test ${#file} -le 16 &&  file_s="$file"
-		echo -n "$file_s"
-		echo -ne '\t'
-		echo -e "$MIM \t$(xdg-mime query default "$MIM")"
+		#https://unix.stackexchange.com/questions/47832/is-there-a-simple-command-for-outputting-tab-delimited-columns/47847#47847
+		file_s=${file: -18}  #cut the filename so columns of output  look ok
+		test ${#file} -le 18  &&  file_s="$file"
+		#`echo -n "$file_s" echo -ne '\t' echo -e "$MIM" \t
+		printf "%-18s      %-33s     %s\n" "$file_s" "$MIM" "$(xdg-mime query default "$MIM")" 
 	done
 	)
 }
@@ -27,3 +28,12 @@ output_mime_code () {
 	done |sort -u
 	)
 }
+
+		
+get_yesno () {
+#perl -e 'if ( <> =~ /^y$/i){ exit 0;} exit 1;'
+#now match any number of yyyy with whitespace
+perl -e 'if ( <> =~ /^\s*y+\s*$/i){ exit 0;} exit 1;'
+
+}
+
