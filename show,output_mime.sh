3f3f2a40c2a1b2f2838eeddc3a9030801159f07d
kanliot <kanliot@gmail.com>
2019-05-03 04:01:25 -0400

    Add files via upload

--- show,output_mime.sh
+++ show,output_mime.sh
@@ -1,16 +1,20 @@
+# pass a list of files to show what xdg-mime thinks about the mime type of the files
 show_mime_from_files () {
 	(
 	for file 
 	#this doesn't work: MIM="$(file -b --mime-type "$file" )"  it appears to work for most files, but returns wrong type for files ending in m4a
 	do MIM="$(xdg-mime query filetype "$file")"
-		file_s=${file: -14}  #cut the filename so columns of output  look ok
+		file_s=${file: -16}  #cut the filename so columns of output  look ok
+		test ${#file} -le 16 &&  file_s="$file"
 		echo -n "$file_s"
 		echo -ne '\t'
-		echo -e "      $MIM \t$(xdg-mime query default "$MIM")"
+		echo -e "$MIM \t$(xdg-mime query default "$MIM")"
 	done
 	)
 }
 
+# pass a list of files, then enter a new desktop file when script asks for it.
+# should output some shellcode to help you change default application for mime types
 output_mime_code () {
 	(
 	echo  'enter code like "mpv.desktop" or ctrl-c'
@@ -23,6 +27,3 @@ output_mime_code () {
 	done |sort -u
 	)
 }
-
-		
-
